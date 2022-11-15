51c948891c3836ca18bfdecb717048da49180128
Lenovo <Lenovo@DESKTOP-GT6R4RE>
2022-11-15 18:01:29 +0530

    First commit

---
+++ github/src/test/java/github/github_class.java
@@ -0,0 +1,12 @@
+package github;
+
+public class github_class {
+public static void main(String[] args) {
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+}
+}

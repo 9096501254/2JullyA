2c9940e645df4fb6584e891125fac99830bd5474
9096501254 <118271911+9096501254@users.noreply.github.com>
2022-11-15 18:33:28 +0530

    Update github_class.java

--- github/src/test/java/github/github_class.java
+++ github/src/test/java/github/github_class.java
@@ -4,9 +4,6 @@ public class github_class {
 public static void main(String[] args) {
 	System.out.println("TC01");
 	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
+	
 }
 }
